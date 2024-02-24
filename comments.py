e745a62765dc70ba1d4bcaec1a58def744e7e58b
rajat3154 <rajaturanvir31@gmail.com>
2024-02-24 23:43:42 +0530

    comments

---
+++ comments.py
@@ -0,0 +1,12 @@
+#comments and print statement in python
+print("Comments in python")
+#this is a single line comment
+print("Types of comments")
+'''This is a multiline comment 
+there are two types of comments in python
+1.Single line comment denoted by # 
+2.Multiline comment denoted by '''  ''' and """  """
+'''
+print("Comments Done")
+
+#print statement in python
